b43eb0f606c83805d1b4c2f13fd2055a65cb374e
nicolas <nicolas.kelepuris@gmail.com>
2021-10-05 10:49:03 -0300

    refactor name validation

--- src/Application/Requests/Users/PostUserRequest.cs
+++ src/Application/Requests/Users/PostUserRequest.cs
@@ -21,7 +21,7 @@ namespace Application.Requests.Users
         public PostUserRequestValidator(UserManager<AppUser> userManager)
         {
             RuleFor(p => p.Name).NotEmpty();
-            RuleFor(p => p.Name).Must(name => !name.Any(c => !Char.IsLetter(c) && !Char.IsWhiteSpace(c)));
+            RuleFor(p => p.Name).Must(name => AllCharactersAreLetterOrWhiteSpace(name));
 
             RuleFor(p => p.Password).NotEmpty();
             var passwordValidator = new PasswordValidator<AppUser>();
@@ -36,6 +36,10 @@ namespace Application.Requests.Users
                 .Must(email => userManager.FindByEmailAsync(email).Result == null)
                 .WithMessage("Email ja cadastrado")
                 .When(p => !string.IsNullOrEmpty(p.Email));
+        }
+
+        private bool AllCharactersAreLetterOrWhiteSpace(string text) {
+            return !text.Any(c => !Char.IsLetter(c) && !Char.IsWhiteSpace(c));
         }
     }
 }
