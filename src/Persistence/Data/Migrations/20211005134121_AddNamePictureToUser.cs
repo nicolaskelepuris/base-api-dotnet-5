f9e1c748c21ad442e609fefa3647cdc21fc26d78
nicolas <nicolas.kelepuris@gmail.com>
2021-10-05 10:41:42 -0300

    add name and picture to user

---
+++ src/Persistence/Data/Migrations/20211005134121_AddNamePictureToUser.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Data.Migrations
+{
+    public partial class AddNamePictureToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Picture",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Picture",
+                table: "AspNetUsers");
+        }
+    }
+}
